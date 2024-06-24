831af90688bc8f4e296fa7985d2d9ee5ff8399e1
rizvan <rizvangaripov@gmail.com>
2024-06-24 10:34:12 +0300

    remove unnecessary code

--- app/src/main/java/com/garif/testapplication/App.kt
+++ app/src/main/java/com/garif/testapplication/App.kt
@@ -1,23 +1,7 @@
 package com.garif.testapplication
 
 import android.app.Application
-import android.util.LruCache
-import com.garif.testapplication.task2.LaunchTimeDelegate
-import com.garif.testapplication.task2.LogLaunchTime
 
 class App() : Application() {
-    private val memoryCache = LruCache<String, Long>(8)
-    private var launchTime: Long by LaunchTimeDelegate(memoryCache)
-    private lateinit var logLaunchTime: LogLaunchTime
 
-    override fun onCreate() {
-        super.onCreate()
-        launchTime = System.currentTimeMillis() - launchTime
-        logLaunchTime = LogLaunchTime(memoryCache)
-    }
-
-    override fun onTerminate() {
-        super.onTerminate()
-        logLaunchTime.stopLogging()
-    }
 }
